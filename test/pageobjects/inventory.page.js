af0cfcf43bbcb161755f91f4e90add6046cc00f6
Surabhi <ksurabhi7861@gmail.com>
2025-11-05 00:01:23 -0700

    added more test cases

--- test/pageobjects/inventory.page.js
+++ test/pageobjects/inventory.page.js
@@ -1,5 +1,6 @@
-import { $ } from '@wdio/globals'
+import { $, expect } from '@wdio/globals'
 import Page from './page.js';
+import loginPage from './login.page.js';
 
 /**
  * Landing page specific selectors and methods for a specific page
@@ -18,7 +19,7 @@ class InventoryPage extends Page {
     }
 
     get OpenCartIcon () {
-        return $('.shopping_cart_link');
+        return $('.shopping_cart_link'); 
     }
 
     get openLogout () {
@@ -29,23 +30,48 @@ class InventoryPage extends Page {
         return $('#about_sidebar_link');
     }
 
-     get yourCartTitle () {
+    get yourCartTitle () {
         return $('.title');
     }
+
     get continueShoppingButton () {
         return $('#continue-shopping');
     }
+
     get checkoutButton () {
         return $('#checkout');
     }
 
+    get addToCartButton(){
+        return $('[id="add-to-cart-sauce-labs-backpack"]');
+    }
+
+    get removeButton(){
+        return $('#remove-sauce-labs-backpack');
+    }
+    
+    get resetAppStateButton(){
+        return $('#reset_sidebar_link');
+    }
+    get allItemsButton(){
+        return $('#inventory_sidebar_link');
+    }
+    get products(){
+        return $('[data-test="title"]');
+    }
+    get bookDemo(){
+        return $('[href="https://saucelabs.com/request-demo"]');
+    }
+    
 // function
     async openHamburger () {
         await this.hamburgerIcon.click();
+        await expect(this.aboutOpen).toHaveText('About');  
     }
 
     async closeHamburger () {
         await this.hamburgerIconClose.click();
+        await expect (loginPage.appLogo).toHaveText('Swag Labs');
     }
 
 
@@ -53,17 +79,33 @@ class InventoryPage extends Page {
         await this.OpenCartIcon.click();
         await expect(this.yourCartTitle).toHaveText('Your Cart'); 
         await expect(this.continueShoppingButton).toHaveText('Continue Shopping');
-        await expect(this.checkoutButton).toHaveText('Checkout');//toHaveText
+        await expect(this.checkoutButton).toHaveText('Checkout');
     }
+
     async openAbout () {
         await this.aboutOpen.click();
+        await expect (this.bookDemo).toHaveText('Book a demo');
     }
+
     async logout () {
         await this.openLogout.click();
+        await expect (loginPage.btnSubmit).toHaveAttribute('value','Login');
+        await expect (loginPage.btnSubmit).toBeDisplayed();
     }
-    //example - await expect(this.appLogo).toHaveText('Swag Labs');
 
-
+    async addToCart() {
+        await this.addToCartButton.click();
+        await expect (this.removeButton).toHaveText('Remove');
+        await expect(this.OpenCartIcon).toHaveText('1');
+    }
+    async allItems() {
+        await this.allItemsButton.click();
+        await expect (this.products).toHaveText('Products');
+    }
+    async reset() {
+        await this.resetAppStateButton.click();
+        await expect (this.OpenCartIcon).toHaveText('');
+    }
 }
 
 export default new InventoryPage();
--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -8,22 +8,31 @@ describe('My inventory page application', () => {
     it('should open menu successfully', async () => {
         await LoginPage.open();
         await LoginPage.login('standard_user', 'secret_sauce');
-        //await InventoryPage.openHamburger(); 
     })
 })
 
-// describe('My inventory', () => {
-//         it('should close successfully', async () => {
-//         await InventoryPage.closeHamburger();
-//     })
-// })
+describe('Open Reset App State', () => {
+        it('should clear all items from cart successfully', async () => { 
+        await InventoryPage.addToCart();
+        await InventoryPage.openHamburger();
+        await InventoryPage.reset();
+    })
+})
 
 describe('Open Cart', () => {
-        it('should open Cart  successfully', async () => {
-            
+        it('should open Cart  successfully', async () => {  
             await InventoryPage.openCart();
     })
 })
+
+describe('Open All Items', () => {
+        it('should show all products successfully', async () => {  
+            await InventoryPage.openHamburger();
+            await InventoryPage.allItems();
+    })
+})
+// 
+
 describe('Open About', () => {
         it('should open About successfully', async () => {
         await InventoryPage.openHamburger();
